644ca4919950e20506a11607d6021474b7f0a5fc
taipham231 <tai15122003311@gmail.com>
2020-11-26 18:20:56 +0700

    update the menu admin

--- admin/menu.php
+++ admin/menu.php
@@ -1,4 +1,4 @@
-                <li><a href="<?=$admin_url?>/category"><i class="fas fa-monument"></i>category_product</a></li>    
+                <li><a href="<?=$admin_url?>/main_page"><i class="fas fa-monument"></i>home</a></li>    
                 <li><a href="<?=$admin_url?>/category"><i class="fas fa-monument"></i>category_product</a></li>    
                 <li><a href="<?=$admin_url?>/category13"><i class="fab fa-product-hunt"></i>product</a></li>
                 <li><a href="<?=$admin_url?>/category123"><i class="fas fa-cart-plus"></i>comment</a></li>
